accbd0eaa60f5b70b894c5d1afd015e72bad8308
Migyas <yansantos0712@gmail.com>
2022-03-23 21:56:56 -0300

    default favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import Grid from '@mui/material/Grid';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+export function App() {
 
-export default App;
+  return (
+    <Grid container >
+      <Grid item xs={12} md={6} lg={12} p={7} style={{background: 'yellowgreen'}}>
+          <h1>Hello World</h1>
+      </Grid>
+      <Grid item xs={3} md={6} lg={12} mt={{xs: 2, sm: 5}} style={{background: 'green'}}>
+          <h1>Hello World</h1>
+      </Grid>
+    </Grid>
+  )
+}
+
